83fb7a54c651358d4b326769766fbbcc9cfe4901
Richard Bolkey <rbolkey@gmail.com>
2013-01-28 21:01:11 -0600

    Adding plugin entry point.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 Gradle Scrooge Plugin
 =====================
 
-This is a Gradle plugin for Scrooge(https://github.com/twitter/scrooge), Twitter's Thrift generator for Scala.
+This is a Gradle plugin for [Scrooge](https://github.com/twitter/scrooge), Twitter's Thrift generator for Scala.
---
+++ src/main/groovy/com/bolkey/gradle/scrooge/ScroogePlugin.groovy
@@ -0,0 +1,12 @@
+package com.bolkey.gradle.scrooge
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class ScroogePlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project target) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
